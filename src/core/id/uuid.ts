c63dfb71235da264e9e4dce358097460103aa17e
Henrique <henrique@sidekick-coder.com>
2025-01-20 18:10:59 -0300

    fix: typo

--- src/core/id/uuid.ts
+++ src/core/id/uuid.ts
@@ -4,7 +4,7 @@ import { Strategy } from './types.js'
 export function createUuidStrategy(): Strategy {
     return {
         name: 'uuid',
-        create() {
+        async create() {
             return crypto.randomUUID()
         },
     }
