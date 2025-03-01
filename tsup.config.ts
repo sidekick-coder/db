47ac3dbd0cde34ffc8022337e008985eb1ce7c3a
Henrique <henrique@sidekick-coder.com>
2025-03-01 11:48:00 -0300

    refactor: optimizing bundle size

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'tsup'
 import path from 'path'
 
 export default defineConfig({
-    entry: ['src/**/**/*.ts'],
+    entry: ['src/**/**/*.ts', 'src/**/**/*.spec.ts'],
     format: ['cjs', 'esm'],
     tsconfig: path.resolve(__dirname, 'tsconfig.json'),
     splitting: false,
-    sourcemap: true,
+    minify: true,
     clean: true,
     treeshake: true,
 })
