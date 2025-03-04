0466a9ce7c56d0ad4f98f26401e53c30738f3a67
Henrique <henrique@sidekick-coder.com>
2025-03-04 07:13:12 -0300

    tests: file provider update tests

--- src/providers/file/provider.ts
+++ src/providers/file/provider.ts
@@ -4,13 +4,18 @@ import { defineProvider } from '@/core/provider/defineProvider.js'
 import { DataProvider } from '@/core/provider/types.js'
 import { query, count } from '@/core/provider/queryArray.js'
 
-import omit from 'lodash-es/omit.js'
 import { createIncrementalStategyFromFile } from '@/core/id/incremental.js'
 import { createIdMaker } from '@/core/id/index.js'
 import { v, validate } from '@/core/validator/index.js'
 import { parsers } from '@/core/parsers/index.js'
 
+import { update } from './update.js'
+import { list } from './list.js'
+import { createFilesystem } from '@/core/filesystem/createFilesystem.js'
+
 export const provider = defineProvider((config, { root }) => {
+    const filesystem = createFilesystem()
+
     const schema = v.object({
         path: v.extras.path(root),
         format: v.optional(v.picklist(['markdown', 'json', 'yaml']), 'markdown'),
@@ -29,131 +34,64 @@ export const provider = defineProvider((config, { root }) => {
         strategies: [createIncrementalStategyFromFile(drive, resolve(path, '.db', 'last_id.json'))],
     })
 
-    const list: DataProvider['list'] = async (options) => {
-        const where = options?.where || {}
-        const exclude = options?.exclude
-        const include = options?.include
-        const limit = options?.limit
-        const page = options?.page || 1
-        const sortBy = options?.sortBy
-        const sortDesc = options?.sortDesc
+    //const find: DataProvider['find'] = async (options) => {
+    //    const { data: items } = await list({
+    //        ...options,
+    //        limit: 1,
+    //    })
+    //
+    //    return items[0] || null
+    //}
 
-        const files = await drive.list(path, {
-            onlyFiles: true,
-        })
+    //const create: DataProvider['create'] = async (options) => {
+    //    const { data } = options
+    //
+    //    const { id: explicitId, ...properties } = data
+    //    const id = explicitId || (await makeId(id_strategy, {}))
+    //    const filename = resolve(path, `${id}.${parser.ext}`)
+    //
+    //    if (await drive.exists(filename)) {
+    //        throw new Error(`Item with id "${id}" already exists`)
+    //    }
+    //
+    //    const content = parser.stringify(properties)
+    //
+    //    await drive.write(filename, content)
+    //
+    //    return {
+    //        id: id,
+    //        ...properties,
+    //    }
+    //}
 
-        const filteredFiles = files.filter((f) => {
-            if (f === '.db') return false
-
-            return true
-        })
-
-        const result = [] as any[]
-
-        for (const file of filteredFiles) {
-            const filename = resolve(path, file)
-
-            const content = drive.readSync(filename)
-
-            const item: any = {
-                id: file.replace(`.${parser.ext}`, ''),
-                filename: filename,
-                raw: content,
-            }
-
-            Object.assign(item, parser.parse(content))
-
-            result.push(item)
-        }
-
-        const items = query(result, {
-            where,
-            exclude,
-            include,
-            limit,
-            offset: page > 1 ? (page - 1) * limit : 0,
-            sortBy,
-            sortDesc,
-        })
-
-        const meta = {
-            total: count(result, { where }),
-            limit,
-            total_pages: limit ? Math.ceil(result.length / limit) : 1,
-        }
-
-        return {
-            meta,
-            data: items,
-        }
-    }
-
-    const find: DataProvider['find'] = async (options) => {
-        const { data: items } = await list({
-            ...options,
-            limit: 1,
-        })
-
-        return items[0] || null
-    }
-
-    const create: DataProvider['create'] = async (options) => {
-        const { data } = options
-
-        const { id: explicitId, ...properties } = data
-        const id = explicitId || (await makeId(id_strategy, {}))
-        const filename = resolve(path, `${id}.${parser.ext}`)
-
-        if (await drive.exists(filename)) {
-            throw new Error(`Item with id "${id}" already exists`)
-        }
-
-        const content = parser.stringify(properties)
-
-        await drive.write(filename, content)
-
-        return {
-            id: id,
-            ...properties,
-        }
-    }
-
-    const update: DataProvider['update'] = async (options) => {
-        const { where, data } = options
-
-        const { data: items } = await list({ where, exclude: [] })
-
-        for (const item of items) {
-            const filename = resolve(path, `${item.id}.${parser.ext}`)
-
-            const hideKeys = ['id', 'filename', 'raw']
-
-            const properties = omit({ ...item, ...data }, hideKeys)
-
-            const content = parser.stringify(properties)
-
-            await drive.write(filename, content)
-        }
-
-        return { count: items.length }
-    }
-
-    const destroy: DataProvider['destroy'] = async (options) => {
-        const { where } = options
-        const { data: items } = await list({ where, include: ['filename'] })
-
-        for (const item of items) {
-            await drive.destroy(item.filename)
-        }
-
-        return { count: items.length }
-    }
+    //const destroy: DataProvider['destroy'] = async (options) => {
+    //    const { where } = options
+    //    const { data: items } = await list({ where, include: ['filename'] })
+    //
+    //    for (const item of items) {
+    //        await drive.destroy(item.filename)
+    //    }
+    //
+    //    return { count: items.length }
+    //}
 
     return {
-        list,
-        find,
-        create,
-        update,
-        destroy,
+        list: (options) =>
+            list({
+                root: config.path,
+                filesystem,
+                parser,
+                options,
+            }),
+        //find,
+        //create,
+        update: (options) =>
+            update({
+                root: config.path,
+                filesystem,
+                options,
+                parser,
+            }),
+        //destroy,
     }
 })
--- src/providers/file/update.spec.ts
+++ src/providers/file/update.spec.ts
@@ -1,3 +1,114 @@
-import { testUpdate } from '@/__tests__/factories/provider.js'
+import { beforeEach, expect, it } from 'vitest'
+import { createFilesystemFake } from '@/core/filesystem/createFilesystemFake.js'
+import { update } from './update.js'
+import { json } from '@/core/parsers/all.js'
 
-testUpdate(async (payload) => {})
+const filesystem = createFilesystemFake()
+
+beforeEach(() => {
+    filesystem.removeSync('/db')
+
+    filesystem.mkdirSync('/db')
+})
+
+it('should update an item in the database', async () => {
+    filesystem.writeSync.json('/db/01.json', {
+        title: 'Hello word',
+    })
+
+    const response = await update({
+        root: '/db',
+        filesystem,
+        parser: json,
+        options: {
+            where: {
+                id: '01',
+            },
+            data: {
+                title: 'Updated',
+            },
+        },
+    })
+
+    expect(response.count).toBe(1)
+
+    const item = filesystem.readSync.json('/db/01.json')
+
+    expect(item).to.containSubset({ title: 'Updated' })
+})
+
+it('should not remove previous properties if not included in update', async () => {
+    filesystem.writeSync.json('/db/01.json', {
+        title: 'Hello word',
+    })
+
+    const response = await update({
+        root: '/db',
+        filesystem,
+        parser: json,
+        options: {
+            where: {
+                id: '01',
+            },
+            data: {
+                type: 'message',
+            },
+        },
+    })
+
+    expect(response.count).toBe(1)
+
+    const item = filesystem.readSync.json('/db/01.json')
+
+    expect(item).to.containSubset({ title: 'Hello word', type: 'message' })
+})
+
+it('should update multiple items with where clause ', async () => {
+    filesystem.writeSync.json('/db/01.json', { title: 'Hello', type: 'greeting' })
+    filesystem.writeSync.json('/db/02.json', { title: 'Bye', type: 'greeting' })
+
+    const response = await update({
+        root: '/db',
+        filesystem,
+        parser: json,
+        options: {
+            where: {
+                type: 'greeting',
+            },
+            data: {
+                is_greeting: true,
+            },
+        },
+    })
+
+    expect(response.count).toBe(2)
+
+    expect(filesystem.readSync.json('/db/01.json')?.is_greeting).toBe(true)
+    expect(filesystem.readSync.json('/db/02.json')?.is_greeting).toBe(true)
+})
+
+it('should not update any items if none match where clause', async () => {
+    filesystem.writeSync.json('/db/01.json', { title: 'Hello word', type: 'message' })
+    filesystem.writeSync.json('/db/02.json', { title: 'Hello', type: 'greeting' })
+    filesystem.writeSync.json('/db/03.json', { title: 'Bye', type: 'greeting' })
+
+    const response = await update({
+        root: '/db',
+        filesystem,
+        parser: json,
+        options: {
+            where: {
+                type: 'greeting',
+            },
+            data: {
+                is_greeting: true,
+            },
+        },
+    })
+
+    expect(response.count).toBe(2)
+
+    expect(filesystem.readSync.json('/db/01.json')?.is_greeting).to.not.toBeDefined()
+    expect(filesystem.readSync.json('/db/02.json')?.is_greeting).toBe(true)
+    expect(filesystem.readSync.json('/db/03.json')?.is_greeting).toBe(true)
+})
