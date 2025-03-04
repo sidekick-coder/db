77f67c26c762d42e508d625b08dace0304e5c9fe
Henrique <henrique@sidekick-coder.com>
2025-03-04 07:47:03 -0300

    refactor: remove tests from build files

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 import path from 'path'
 
 export default defineConfig({
-    entry: ['src/**/**/*.ts', 'src/**/**/*.spec.ts'],
+    entry: ['src/**/**/*.ts', '!src/**/**/*.spec.ts'],
     format: ['cjs', 'esm'],
     tsconfig: path.resolve(__dirname, 'tsconfig.json'),
     splitting: false,
